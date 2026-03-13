a1ea7d4c3e106554665a01d0275bbc465771aa36
Can Yang <13303311859@163.com>
2026-03-14 09:48:01 +1100

    fix(build): skip tauri build script for sidecar bins

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    if let Ok(bin_name) = std::env::var("CARGO_BIN_NAME") {
+        if bin_name != "dashdrop" {
+            println!("cargo:rerun-if-env-changed=CARGO_BIN_NAME");
+            return;
+        }
+    }
     tauri_build::build()
 }
